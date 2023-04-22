6e160c5a95ec3d22cc289a1939d8b36f0eac958f
Tawanda charuka <tawandalloydcharuka@gmail.com>
2023-04-22 08:58:33 +0200

    removed docker files

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,21 +10,6 @@ router.post('/forgotpassword',authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 router.patch('/updatePassword', authController.protect, authController.updatePassword);
 
-// router.patch('/updateMe', authController.protect , userController.updateMe);
-// router.delete('/deleteMe', authController.protect , userController.updateMe);
-
-// router
-// .route('/')
-// .get(userController.getAllUsers)
-// .post(userController.createNewUser)
-
-// router
-// .route('/:id')
-// .get(userController.getUser)
-// .patch(userController.updateUser)
-// .delete(userController.deleteUser)
-
-
 router
 .route('/')
 .get(userController.getUsers)
