dd65bf74a1d1eb77b33a56defb8822582086a646
Tawanda charuka <tawandalloydcharuka@gmail.com>
2023-04-22 09:43:19 +0200

    still testing

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,7 +21,7 @@ const createSendToken = (user, statusCode, res) => {
       expires: new Date(
         Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
       ),
-      httpOnly: false
+      httpOnly: true
     };
     res.cookie('jwt', token, cookieOptions);
   
